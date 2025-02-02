b45acdcb60e5df0bc6e1db1f292206d21edc14d4
Iam3042 <shreeman.anuj@gmail.com>
2025-02-02 10:12:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,22 +7,22 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = API_ID
-API_HASH = "API_HASH"
+API_ID = 26090790
+API_HASH = "fbe01ca61b6a977f85dc57a80fd2e7cb"
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = "BOT_TOKEN"
+BOT_TOKEN = "8058270700:AAHn0wZAV2oyLn_2y6UpWQGApCuvFKklSeY"
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = "MONGO_DB_URI"
+MONGO_DB_URI = "mongodb+srv://shreemananuj:shreemananuj@cluster0.72mam.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = LOG_GROUP_ID
+LOG_GROUP_ID = -1002401044938
 
 # Get this value from @ultron2_robot on Telegram by /id
-OWNER_ID = OWNER_ID
+OWNER_ID = 7138928141
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = "https://t.me/thanos_pro"
-SUPPORT_GROUP = "https://t.me/thanosprosss"
+SUPPORT_CHANNEL = "https://t.me/SnarkSanctum"
+SUPPORT_GROUP = "https://t.me/SnarkSanctum"
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -62,7 +62,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
 
 # Get your pyrogram v2 session from Replit
-STRING1 = "STRING_SESSION"
+STRING1 = "BQGOHSYAIsjQ3nexRRfYhq8mOLkgt5PlwuH0VaWlGpxDZNL2KTQU806PoPIIHTHNohy45s30volbRHBZZtaN8PZthQrq1rPUgQcyWnJvmW8VjBnk5kPpq_TmcZtKGDV1L_VJc-egGiuLEBUidtUi-rCeIgt5CsruQgCydzkLXE1crq_a5IA0iJmsFGsGfozBk5SGMpg7LBdkrQiyGYVUP15pgJdY7DkCHXRxSxjg9HY4E_4T1PVMaqJabajmfSzXg5KEP4hVuifwXM47KCOzcppKryW3rajlIW0bxCjI3pHQAUsJTxQZbCLnkWOzvx6mckdU2--eibmVq_5LauQVIBBZlB3KRQAAAAGpg2YNAA"
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -77,19 +77,19 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
+START_IMG_URL = "https://graph.org/file/b1ecc259bc1145717053f-71be50868489f04316.jpg"
 
-PING_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
+PING_IMG_URL = "https://graph.org/file/b1ecc259bc1145717053f-71be50868489f04316.jpg"
 
-PLAYLIST_IMG_URL = "https://graph.org/file/763a841a2ad5cbb1e2fc5.jpg"
-STATS_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
-TELEGRAM_AUDIO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-TELEGRAM_VIDEO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/b1ecc259bc1145717053f-71be50868489f04316.jpg"
+STATS_IMG_URL = "https://graph.org/file/b1ecc259bc1145717053f-71be50868489f04316.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/b1ecc259bc1145717053f-71be50868489f04316.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/b1ecc259bc1145717053f-71be50868489f04316.jpg"
+STREAM_IMG_URL = "https://graph.org/file/b1ecc259bc1145717053f-71be50868489f04316.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/b1ecc259bc1145717053f-71be50868489f04316.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/b1ecc259bc1145717053f-71be50868489f04316.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/b1ecc259bc1145717053f-71be50868489f04316.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/b1ecc259bc1145717053f-71be50868489f04316.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
 
 
